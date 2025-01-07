876e8d089d06bd329f29f5c80d6ee86877497156
Aquerr <nerdianin@gmail.com>
2025-01-07 19:21:03 +0100

    Remove AtomicBoolean from UpdateUserRolesListener

--- src/main/java/pl/tispmc/wolfie/discord/application/listener/UpdateUserRolesListener.java
+++ src/main/java/pl/tispmc/wolfie/discord/application/listener/UpdateUserRolesListener.java
@@ -8,27 +8,18 @@ import org.springframework.stereotype.Component;
 import pl.tispmc.wolfie.common.event.model.UpdateUserRolesEvent;
 import pl.tispmc.wolfie.discord.service.DiscordUserRolesUpdater;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class UpdateUserRolesListener
 {
-    private static final AtomicBoolean UPDATE_IN_PROGRESS = new AtomicBoolean(false);
     private final DiscordUserRolesUpdater userRolesUpdater;
 
     @Async
     @EventListener
     public void onUpdateUserRoles(UpdateUserRolesEvent event)
     {
-        log.info("Got update user roles event.");
-        if (UPDATE_IN_PROGRESS.get()) {
-            log.info("User roles update already in progress. Skipping additional execution.");
-            return;
-        }
-
+        log.info("Scheduling user roles update for: {}", event.getUsersIdsToUpdate());
         this.userRolesUpdater.scheduleRolesUpdate(event.getUsersIdsToUpdate());
-        UPDATE_IN_PROGRESS.set(false);
     }
 }
