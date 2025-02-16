02af1281c3c188372772cf6676c5553c999553fc
Aquerr <nerdianin@gmail.com>
2025-02-16 12:58:21 +0100

    Add Set exp subcommand

--- src/main/java/pl/tispmc/wolfie/discord/command/ExpCommand.java
+++ src/main/java/pl/tispmc/wolfie/discord/command/ExpCommand.java
@@ -23,12 +23,13 @@ import java.util.Set;
 
 @RequiredArgsConstructor
 @Component
-public class AddExpCommand implements SlashCommand
+public class ExpCommand implements SlashCommand
 {
     private static final String MEMBER_PARAM = "member";
     private static final String EXP_PARAM = "exp";
 
-    private static final String SUBCOMMAND_EXP = "add";
+    private static final String SUBCOMMAND_ADD = "add";
+    private static final String SUBCOMMAND_SET = "set";
 
     private final SlashCommandPrerequisites prerequisites;
     private final UserDataService userDataService;
@@ -38,15 +39,19 @@ public class AddExpCommand implements SlashCommand
     public SlashCommandData getSlashCommandData()
     {
         return SlashCommand.super.getSlashCommandData()
-                .addSubcommands(new SubcommandData(SUBCOMMAND_EXP, "Add exp to user")
-                        .addOption(OptionType.USER, MEMBER_PARAM, "Wybierz gracza", true)
-                        .addOption(OptionType.INTEGER, EXP_PARAM, "Podaj wartość", true));
+                .addSubcommands(
+                        new SubcommandData(SUBCOMMAND_ADD, "Dodaj exp graczowi")
+                                .addOption(OptionType.USER, MEMBER_PARAM, "Wybierz gracza", true)
+                                .addOption(OptionType.INTEGER, EXP_PARAM, "Podaj wartość", true),
+                        new SubcommandData(SUBCOMMAND_SET, "Ustaw exp gracza")
+                                .addOption(OptionType.USER, MEMBER_PARAM, "Wybierz gracza", true)
+                                .addOption(OptionType.INTEGER, EXP_PARAM, "Podaj wartość", true));
     }
 
     @Override
     public List<String> getAliases()
     {
-        return List.of("addexp");
+        return List.of("exp");
     }
 
     @Override
@@ -58,9 +63,13 @@ public class AddExpCommand implements SlashCommand
     @Override
     public void onSlashCommand(SlashCommandInteractionEvent event) throws CommandException
     {
-        if (event.getSubcommandName().equals(SUBCOMMAND_EXP))
+        if (event.getSubcommandName().equals(SUBCOMMAND_ADD))
         {
             handleAddExp(event);
+        }
+        else if (event.getSubcommandName().equals(SUBCOMMAND_SET))
+        {
+            handleSetExp(event);
         }
     }
 
@@ -80,6 +89,31 @@ public class AddExpCommand implements SlashCommand
         }
 
         UserData updatedUserData = userData.toBuilder().exp(userData.getExp() + exp).build();
+        userDataService.save(updatedUserData);
+
+        applicationEventPublisher.publishEvent(new UpdateUserRolesEvent(this, Set.of(user.getIdLong())));
+        replyCallbackAction.setEmbeds(new EmbedBuilder()
+                .setColor(Color.RED)
+                .setDescription("Zaktualizowano exp dla " + user.getEffectiveName())
+                .build()).queue();
+    }
+
+    private void handleSetExp(SlashCommandInteractionEvent event)
+    {
+        ReplyCallbackAction replyCallbackAction = event.deferReply();
+        if (!prerequisites.hasGameMasterRole(event.getMember()))
+            throw new CommandException("Brak wymaganej roli do użycia tej komendy.");
+
+        Member user = event.getOption(MEMBER_PARAM).getAsMember();
+        int exp = event.getOption(EXP_PARAM).getAsInt();
+
+        UserData userData = userDataService.find(user.getIdLong());
+        if (userData == null)
+        {
+            userData = createNewUserData(user.getUser());
+        }
+
+        UserData updatedUserData = userData.toBuilder().exp(exp).build();
         userDataService.save(updatedUserData);
 
         applicationEventPublisher.publishEvent(new UpdateUserRolesEvent(this, Set.of(user.getIdLong())));
