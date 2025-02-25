59b1863ae84093530fba8e5b569b2fdd14161d27
Aquerr <nerdianin@gmail.com>
2025-02-25 14:25:10 +0100

    Allow commands to be executed in certain channel only

--- src/main/java/pl/tispmc/wolfie/discord/listener/SlashCommandEventListener.java
+++ src/main/java/pl/tispmc/wolfie/discord/listener/SlashCommandEventListener.java
@@ -1,22 +1,36 @@
 package pl.tispmc.wolfie.discord.listener;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pl.tispmc.wolfie.discord.command.CommandManager;
 import pl.tispmc.wolfie.discord.command.SlashCommand;
 
+import java.awt.*;
+
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SlashCommandEventListener extends ListenerAdapter
 {
     private final CommandManager commandManager;
 
+    @Value("${bot.channels.commands.id:0}")
+    private int commandsChannelId;
+
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event)
     {
+        if (event.getChannel().getIdLong() != commandsChannelId)
+        {
+            showWrongChannelMessage(event);
+            return;
+        }
+
         for (final SlashCommand slashCommand : this.commandManager.getSlashCommands())
         {
             if (slashCommand.supports(event))
@@ -25,5 +39,18 @@ public class SlashCommandEventListener extends ListenerAdapter
                 break;
             }
         }
+    }
+
+    private void showWrongChannelMessage(SlashCommandInteractionEvent event)
+    {
+        MessageEmbed embed = new EmbedBuilder()
+                .setColor(Color.RED)
+                .setDescription(":john_wick_angry: Nie ten kanał, zmykaj bo Cię ugryzzzę! :bob_ross_gun:")
+                .build();
+
+        event.deferReply()
+                .setEphemeral(true)
+                .addEmbeds(embed)
+                .queue();
     }
 }
