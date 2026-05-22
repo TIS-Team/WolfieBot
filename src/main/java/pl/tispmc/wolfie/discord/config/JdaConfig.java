c7919e6db010b0fbd1f79520a52d699ed08e4689
Aquerr <nerdianin@gmail.com>
2026-05-22 11:41:41 +0200

    Enable CacheFlag.SCHEDULED_EVENTS

--- src/main/java/pl/tispmc/wolfie/discord/config/JdaConfig.java
+++ src/main/java/pl/tispmc/wolfie/discord/config/JdaConfig.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.tispmc.wolfie.discord.listener.BotReadyEventListener;
@@ -30,6 +31,7 @@ public class JdaConfig
                    SilentChannelMessageListener silentChannelMessageListener)
     {
         return JDABuilder.createDefault(botConfig.getToken())
+                .enableCache(CacheFlag.SCHEDULED_EVENTS)
                 .enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES, GatewayIntent.SCHEDULED_EVENTS, GatewayIntent.MESSAGE_CONTENT)
                 .setMemberCachePolicy(MemberCachePolicy.ALL)
                 .setChunkingFilter(ChunkingFilter.ALL)
